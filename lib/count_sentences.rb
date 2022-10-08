d86994fe157f0e9897475da3004834e6eaf7c962
Bobby Hinton <bob.g.hint@gmail.com>
2022-10-07 21:40:40 -0500

    commit tree 08f22e2653b776cfb24b645b71949f958993ef3e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") then true
+    else false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") then true
+    else false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") then true
+    else false
+    end 
   end
 
   def count_sentences
-
+    sentence_split = self.split(/[!.?]/)
+    sentence_split.reject { |c| c.empty? }.size
   end
 end
+
+# what does a sentence end with? a period, exclamation, or a question mark
+
+# as such, we should remove all other punctuation
+# then split the string based 
